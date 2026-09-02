16d640dd961dbb5b7b69c1f74a4e7cf08d804174
Mary-Clare Saibert <127303402+Marieclairet@users.noreply.github.com>
2026-09-02 23:15:02 +0000

    Update CORS allowed origins
    Added a new allowed origin for CORS configuration.

--- smart_security_system-main/Backend/src/server.js
+++ smart_security_system-main/Backend/src/server.js
@@ -18,9 +18,10 @@ app.use(
     origin: function (origin, callback) {
       if (!origin) return callback(null, true);
       const allowed = [
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-      ];
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "https://intelliguard-nilqwnz7o-marieclairet.vercel.app",
+];
       if (allowed.includes(origin)) return callback(null, true);
       return callback(new Error("Not allowed by CORS"));
     },
