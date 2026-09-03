42a4d16e65dbf166533e3af98d6bd0c805cc95c1
Mary-Clare Saibert <127303402+Marieclairet@users.noreply.github.com>
2026-09-03 01:30:45 +0000

    Update server.js

--- smart_security_system-main/Backend/src/server.js
+++ smart_security_system-main/Backend/src/server.js
@@ -20,7 +20,7 @@ app.use(
       const allowed = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "https://intelliguard-nilqwnz7o-marieclairet.vercel.app",
+ "https://intelliguard-seven.vercel.app",
 ];
       if (allowed.includes(origin)) return callback(null, true);
       return callback(new Error("Not allowed by CORS"));
